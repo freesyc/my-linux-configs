af8d5c49061d6f113689cc887c88833ac06fd8e8
lnz222 <lnz222@protonmail.com>
2021-10-06 14:48:20 -0300

    script used to reset xfce panel to default configurations

---
+++ reset-panel.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+xfce4-panel --quit && echo "Shut down the panel"
+pkill xfconfd && echo "Kill the xfce4 configuration daemon"
+rm -rf ~/.config/xfce4/panel && echo "Delete settings for the panel"
+rm -rf ~/.config/xfce4/xfconf/xfce-perchannel-xml/xfce4-panel.xml && echo "Clear out the settings for xfconfd"
+xfce4-panel && echo "Restart the panel"
