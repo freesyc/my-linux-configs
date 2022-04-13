fcaff6ff5b3cd17d66674e14eac3c179f85203eb
luan-adao <luanadao98@gmail.com>
2022-04-13 09:16:53 -0300

    arquivos deletados, para efetuar a reestruturação

--- disable-beep.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-rmmod pcspkr
-echo "blacklist pcspkr" > /etc/modprobe.d/blacklist.conf
--- disable-snap.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-rm -rf /var/cache/snapd/ && echo "Clear snap cache"
-apt autoremove --purge snapd gnome-software-plugin-snap && echo "Uninstall snap and snap GUI tool"
-rm -rf ~/snap && echo "Clear snap preferences"
-apt-mark hold snapd && echo "Put snap on hold"
-echo "Finished"
