4c63e6893261c5e93eaf8c14a419bdc400312f80
luan-adao <luanadao98@gmail.com>
2022-04-16 01:48:36 -0300

    add xfce4-docklike-plugin && xfce4-places-plugin packages

--- manjaro/pkgs2install.sh
+++ manjaro/pkgs2install.sh
@@ -5,5 +5,5 @@ if [[ $EUID != 0 ]]; then
 	exit 1
 fi
 
-pkgs="kdeconnect redshift qutebrowser klavaro code git github-cli git-cola"
+pkgs="kdeconnect redshift qutebrowser klavaro code git github-cli git-cola xfce4-docklike-plugin xfce4-places-plugin"
 pamac install $pkgs --no-confirm --no-upgrade
