609665bb99efb48c417b59491f44f410ed94d970
luan-adao <luanadao98@gmail.com>
2022-07-25 11:29:07 -0300

    Deletados scripts usados no Manjaro

--- manjaro/pkgs2install.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-if [[ $EUID != 0 ]]; then
-	echo "Run  as root"
-	exit 1
-fi
-
-pkgs="kdeconnect redshift qutebrowser klavaro code git github-cli git-cola xfce4-docklike-plugin xfce4-places-plugin"
-pamac install $pkgs --no-confirm --no-upgrade
--- manjaro/pkgs2remove.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-if [[ $EUID != 0 ]]; then
-	echo "run as root"
-	exit 1
-fi
-
-pamac remove pidgin --no-confirm
-pamac remove hexchat --no-confirm
